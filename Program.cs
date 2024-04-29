1c35a35ccde51d423c6a0056d44eac079f779ada
PGgamer2 <38359416+PGgamer2@users.noreply.github.com>
2024-04-29 11:07:33 +0200

    Start only if parameter requirements are met

--- Program.cs
+++ Program.cs
@@ -8,17 +8,30 @@ namespace sethc.exe
 	static class Program
 	{
 		[STAThread]
-		static void Main()
+		static void Main(string[] args)
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
+			// Check if argument is valid
+			if (Debugger.IsAttached) {
+				args = new string[] { "211" };
+			} else {
+				try
+				{
+					short arg = short.Parse(args[0]);
+					if (arg < 211 || arg > 251) return;
+					if ((arg - 211) % 10 != 0) return;
+				}
+				catch (Exception) { return; }
+			}
 
 			// Don't open the window if the process already exists
 			string thisprocessname = Process.GetCurrentProcess().ProcessName;
 			if (Process.GetProcesses().Count(p => p.ProcessName == thisprocessname) > 1)
 				return;
 
-			Application.Run(new Main());
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			Application.Run(new Main(args));
 		}
 	}
 }
